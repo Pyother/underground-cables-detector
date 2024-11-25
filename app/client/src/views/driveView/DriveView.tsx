cd3e82435d857f12f01052b8387b03bc55a6f24c
pyother <piotr.dominik.sobol@gmail.com>
2024-11-25 17:02:27 +0100

    Frontend - map view layout

--- app/client/src/views/driveView/DriveView.tsx
+++ app/client/src/views/driveView/DriveView.tsx
@@ -1,11 +1,52 @@
+// * React and Redux:
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../store/store';
+
+// * CSS:
+import './driveView.css';
+
+// * MUI:
+import {
+    Grid,
+    Stack,
+    Select
+} from '@mui/material';
 
 const DriveView = () => {
 
+    const deviceType = useSelector((state: RootState) => state.deviceType.deviceType);
+
     return (
-        <div>
-            DriveView
-        </div>
+        <>
+            {
+                deviceType === 'mobile' || deviceType === 'tablet' ?
+                <Stack style={{height: '100%'}}>
+                    <div className="drive-view-select-container">
+                        <Select>
+
+                        </Select>
+                    </div>
+                    <div className="drive-view-cell">
+                        Wybór
+                    </div>
+                    <div className="drive-view-cell">
+                        Kontroler
+                    </div>
+                </Stack> :
+                <Grid container className="drive-view-container">
+                    <Grid item md={6} lg={6} className="drive-view-cell center">
+                        Mapa
+                    </Grid>
+                    <Grid item md={6} lg={6}>
+                        <Stack style={{height: '100%'}}> 
+                            <div className="drive-view-cell center">Podgląd wideo</div>
+                            <div className="drive-view-cell center">Kontroler</div>
+                        </Stack>
+                    </Grid>
+                </Grid>
+            }
+        </>
     )
 }
 
